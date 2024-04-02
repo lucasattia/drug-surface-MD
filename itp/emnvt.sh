0b0a03b12833b1243971816e2629564130abfbfc
Lucas Attia <43359170+lucasattia@users.noreply.github.com>
2024-04-02 16:54:22 -0400

    Add files via upload

---
+++ itp/emnvt.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 20
+
+
+
+source /etc/profile
+source $HOME/gromacs/gromacs-2021/install/bin/GMXRC
+./etc/profile.d/modules.sh
+module load mpi/openmpi-4.0
+module load anaconda/2021b
+gmx_mpi grompp -f em.mdp -c fens_mt.gro -n fens_mt.ndx -p fens.top -o em.tpr -r fens_mt.gro
+gmx_mpi mdrun -v -deffnm em -ntomp 20
+gmx_mpi grompp -f nvt.mdp -c em.gro -n fens_mt.ndx -p fens.top -o nvt.tpr -r em.gro
+gmx_mpi mdrun -v -deffnm nvt -ntomp 20
+sbatch extendnvt.sh
+exit;
+
---
+++ itp/insert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 24
+
+
+
+source /etc/profile
+source $HOME/gromacs/gromacs-2021/install/bin/GMXRC
+./etc/profile.d/modules.sh
+module load mpi/openmpi-4.0
+module load anaconda/2021b
+gmx_mpi insert-molecules -f fens.gro -o fens_mcaniso.gro -ip mcaniso_pos.dat -dr 9 9 1.25 -radius 0.01 -nmol 53 -try 1000000 -ci MC.pdb -rot z
+gmx_mpi insert-molecules -f fens_mcaniso.gro -o fens_m.gro -ip mciso_pos.dat -dr 9 9 1.25 -radius 0.01 -nmol 54 -try 1000000 -ci MC.pdb
+gmx_mpi insert-molecules -f fens_m.gro -o fens_mt.gro -ip tw80_pos.dat -dr 10 10 1.25 -radius 0.01 -nmol 7 -try 1000000 -ci tw80.pdb
+
+exit;
+
